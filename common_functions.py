4e0d64cc1c847d8dd848ab0854c68a2fae3d4524
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-05-25 04:13:10 +0900

    added some std out and modified parameters for remote docker engine.

--- common_functions.py
+++ common_functions.py
@@ -4,7 +4,7 @@ import docker
 def main():
     # to manage remote docker host, use APIClient class in docker-py, not docker.from_env()
     # http://docker-py.readthedocs.io/en/stable/api.html#module-docker.api.container
-    client = docker.APIClient(base_url='tcp://10.62.130.166:2375', tls=False, version='1.37')
+    client = docker.APIClient(base_url='tcp://10.0.10.100:2375', tls=False, version='1.37')
     images = client.images()
     container_list = client.containers()
     print('Before : ' +str(len(container_list)))
@@ -18,11 +18,13 @@ def main():
 
     # show running containers
     container_ids = []
+    print('==== Listing up containers running on Remote Docker Engine...')
     for i in range(len(container_list)):
         print('ContainerID : ' + str(container_list[i]['Id']) + ' ||| ContainerName : ' + str(container_list[i]['Names']))
         container_ids.append(str(container_list[i]['Id']))
 
     # Stop all containers
+    print('==== Stopping containers running on Remote Docker Engine...')
     for container_id in container_ids:
         client.stop(container=container_id)
     container_list = client.containers()
@@ -30,6 +32,7 @@ def main():
 
     # Remove all containers(Force stopped)
     container_list = client.containers(all=True)
+    print('==== Removing containers running on Remote Docker Engine...')
     container_ids = []
     for i in range(len(container_list)):
         print('ContainerID : ' + str(container_list[i]['Id']) + ' ||| ContainerName : ' + str(container_list[i]['Names']))
