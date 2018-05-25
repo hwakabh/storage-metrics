340d5b828797f026cc1f37b643b7f854d9aff128
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-05-25 16:47:33 +0900

    implemented error-handling when sending message to rabbitmq.

--- common_functions.py
+++ common_functions.py
@@ -6,15 +6,25 @@ import params as param
 # RabbitMQ : each collector is 'Producer' and would 'publish(=Send)' message
 def send_message(msg):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=param.mq_address))
-    channel = connection.channel()
-
-    # create channel to send message
-    # TODO: Error-handle if failed declare
-    channel.queue_declare(queue=param.mq_quename)
-    print('FOR_DEBUG>>> Create channel ' + param.mq_quename + ' with RabbitMQ...')
-
-    channel.basic_publish(exchange='', routing_key=param.mq_quename, body=msg)
-    print('[x] Sent \'' + msg + '\'')
+    return_value = False
+    try:
+        channel = connection.channel()
+        # create channel to send message
+        channel.queue_declare(queue=param.mq_quename)
+        print('FOR_DEBUG>>> Create channel ' + param.mq_quename + ' with RabbitMQ...')
+        channel.basic_publish(exchange='', routing_key=param.mq_quename, body=msg)
+        print('FOR_DEBUG>>> [x]Messaage \'' + msg + '\' successfully sent to ' + param.mq_quename)
+        return_value = True
+    except Exception as e:
+        print('LOGGER>>> Error when sending message ...')
+        print('Errors : ', e.args)
+    finally:
+        try:
+            connection.close()
+        except Exception as e:
+            print('LOGGER>>> Error when sending message ...')
+            print('Errors : ', e.args)
+    return return_value
 
 
 def main():
