67c029f10af6643759b29cb8cf0abd7641ddddb7
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-05-25 10:55:04 +0900

    moved docker class to controller's functions because it would be used only by controller.

--- common_functions.py
+++ common_functions.py
@@ -1,60 +1,9 @@
-# to manage remote docker host, use APIClient class in docker-py, not docker.from_env()
-# http://docker-py.readthedocs.io/en/stable/api.html#module-docker.api.container
-import docker
-import params as param
-
-
-class Common:
-    def __init__(self):
-        self.docker_ip = param.docker_ip
-        self.docker_port = param.docker_port
-        self.docker_api_version = param.docker_api_version
-        self.target = 'tcp://' + self.docker_ip + ':' + str(self.docker_port)
-        self.client = docker.APIClient(base_url=self.target, tls=False, version=self.docker_api_version)
-
-    def get_containers(self, isall):
-        if isall:
-            container_list = self.client.containers(all=True)
-            msg = 'FOR_DEBUG>>> Listing up ALL containers on Remote Docker Engine...'
-        else:
-            container_list = self.client.containers()
-            msg = 'FOR_DEBUG>>> Listing up containers RUNNING on Remote Docker Engine...'
-
-        print(msg)
-        container_ids = {}
-        for i in range(len(container_list)):
-            container_ids[str(container_list[i]['Names'][0]).replace('/','')] = str(container_list[i]['Id'])
-        # for k, v in container_ids.items():
-        #     print('ContainerName : ' + k + '\t ID : ' + v)
-        return container_ids
-
-    def get_container_id(self, strmark):
-        containers = self.get_containers(isall=True)
-        if strmark in containers:
-            return containers.get(strmark)
-        else:
-            print('FOR_DEBUG>>> Container seems to be not existed ...')
-            return None
-
-    def launch_container(self, strmark, cname):
-        image_name = strmark + ':latest'
-        print('>>> Creating ' + strmark + ' containers and starting it ...')
-        c = self.client.create_container(image=image_name, detach=True, name=cname)
-        self.client.start(c)
-
-    # Delete container by name(strmark)
-    def kill_container(self, strmark, isremove):
-        container_id = self.get_container_id(strmark)
-        if isremove:
-            print('>>> Removing ' + strmark + '(ID: ' + container_id + ') ...')
-            self.client.remove_container(container=container_id, force=True)
-        else:
-            print('>>> Stopping ' + strmark + '(ID: ' + container_id + ') ...')
-            self.client.stop(container=container_id)
+# common functions for each collector
 
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
