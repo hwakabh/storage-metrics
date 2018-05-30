07d001a6ac87c79dd0258c1cc9196ed24d42cb20
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-05-30 18:17:39 +0900

    create class for each collectors, and modify usages.

--- common_functions.py
+++ common_functions.py
@@ -3,6 +3,8 @@ import pika
 import requests
 import json
 import params as param
+import psycopg2
+
 
 # To hide InsecureRequesetWarning in prompt
 import urllib3
@@ -10,30 +12,96 @@ from urllib3.exceptions import InsecureRequestWarning
 urllib3.disable_warnings(InsecureRequestWarning)
 
 
-# RabbitMQ : each collector is 'Producer' and would 'publish(=Send)' message
-def send_message(msg):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=param.mq_address))
-    return_value = False
-    try:
-        channel = connection.channel()
-        # create channel to send message
-        channel.queue_declare(queue=param.mq_quename)
-        print('FOR_DEBUG>>> Create channel ' + param.mq_quename + ' with RabbitMQ...')
-        channel.basic_publish(exchange='', routing_key=param.mq_quename, body=msg)
-        print('FOR_DEBUG>>> [x]Messaage \'' + msg + '\' successfully sent to ' + param.mq_quename)
-        return_value = True
-    except Exception as e:
-        print('LOGGER>>> Error when sending message ...')
-        print('Errors : ', e.args)
-    finally:
+# Postgres Class to handle in each function
+class Postgres:
+    def __init__(self, hostname, port, username, password, database):
+        self.hostname = hostname
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+
+    def connect(self):
+        self.connection = psycopg2.connect(host=self.hostname, user=self.username, password=self.password,
+                                           dbname=self.database, port=self.port)
+        self.connection.autocommit = True
+        return self.connection
+
+    def get_connection(self):
+        return self.connection
+
+    def disconnect(self):
+        self.connection.close()
+
+
+def create_table(name, columns):
+    # Instantiate Postgres
+    pg = Postgres(hostname=param.pg_address, port=param.pg_ports[0],
+                  username=param.pg_username, password=param.pg_password, database=param.pg_database)
+    # Generate query
+    q = 'CREATE TABLE IF NOT EXISTS ' + name + columns
+    # Connect to postgres and run create query
+    pg.connect()
+    cur = pg.get_connection().cursor()
+    cur.execute(q)
+    pg.disconnect()
+
+
+def run_sql_query():
+    pass
+
+
+def send_data_to_postgres():
+    pass
+
+
+class Collector:
+    def __init__(self, strmark):
+        self.strmark = strmark
+
+    def create_capacity_table(self, columns):
+        table_name = self.strmark + '_capacity_table'
+        print(self.strmark + '_LOGGER>>> Creating capacity tables on Postgres ...')
+        create_table(table_name, columns)
+        print(self.strmark + '_LOGGER>>> Creating capacity tables on Postgres ...')
+
+    def create_quota_table(self, columns):
+        table_name = self.strmark + '_quota_table'
+        print(self.strmark + '_LOGGER>>> Creating quota tables on Postgres ...')
+        create_table(table_name, columns)
+        print(self.strmark + '_LOGGER>>> Creating quota tables Done.')
+
+    def create_performance_table(self, columns):
+        table_name = self.strmark + '_performance_table'
+        print(self.strmark + '_LOGGER>>> Creating performance tables on Postgres ...')
+        create_table(table_name, columns)
+        print(self.strmark + '_LOGGER>>> Creating performance tables Done.')
+
+    # RabbitMQ : each collector is 'Producer' and would 'publish(=Send)' message
+    def send_message(self, msg):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=param.mq_address))
+        return_value = False
         try:
-            connection.close()
+            channel = connection.channel()
+            # create channel to send message
+            channel.queue_declare(queue=param.mq_quename)
+            print(self.strmark + '_LOGGER>>> Create channel ' + param.mq_quename + ' with RabbitMQ...')
+            channel.basic_publish(exchange='', routing_key=param.mq_quename, body=msg)
+            print(self.strmark + '_LOGGER>>> [x]Messaage \'' + msg + '\' successfully sent to ' + param.mq_quename)
+            return_value = True
         except Exception as e:
-            print('LOGGER>>> Error when sending message ...')
+            print(self.strmark + '_LOGGER>>> Error when sending message ...')
             print('Errors : ', e.args)
-    return return_value
+        finally:
+            try:
+                connection.close()
+            except Exception as e:
+                print(self.strmark + '_LOGGER>>> Error when sending message ...')
+                print('Errors : ', e.args)
+        return return_value
 
 
+# Utilities
 def convert_to_json(rbody):
     js = ''
     try:
@@ -67,27 +135,9 @@ def get_https_response_with_json(username, password, url):
     return convert_to_json(rbody)
 
 
-def get_postgres_connection():
-    pass
-
-
-def run_sql_query():
-    pass
-
-
-def create_table():
-    pass
-
-
-def send_data_to_postgres():
-    pass
-
-
 def main():
-    send_message('postgres_Start')
-    send_message('rabbit_Start')
-    send_message('xtremio_Start')
-    send_message('isilon_Start')
+    test_collector = Collector('TEST')
+    test_collector.send_message('postgres_Start')
 
 
 if __name__ == '__main__':
--- isilon_collector.py
+++ isilon_collector.py
@@ -1,4 +1,5 @@
-import common_functions as common
+from common_functions import Collector
+from common_functions import get_https_response_with_json
 import params as param
 
 global str_ipaddress
@@ -7,9 +8,11 @@ global str_password
 
 
 def get_isilon_information(ip, user, passwd):
-    uri = 'https://' + ip + ':8080' + '/platform/1/cluster/config'
+    api = 'https://' + ip + ':8080' + '/platform/1/cluster/config'
     try:
-        ret = common.get_https_response_with_json(user, passwd, uri)
+        # Execute HTTPS GET
+        ret = get_https_response_with_json(user, passwd, api)
+        # Printing for debug
         print('S/N : ' + ret['local_serial'])
         print('Cluster Name : ' + ret['name'])
         print('OneFS Version : ' + ret['onefs_version']['release'] + ' <<Build : ' + ret['onefs_version']['build'] + '>>')
@@ -17,7 +20,10 @@ def get_isilon_information(ip, user, passwd):
         print('Nodes information : ')
         for d in ret['devices']:
             print('DeviceID : ' + str(d['devid']) + ' <<GUID : ' + d['guid'] + '>>')
+        # Return result
+        return ret
     except Exception:
+        # TODO: Specify Exception if wrong URL
         print('ISILON_LOGGER>>> Exception is throwed by common function. '
               'Error when getting information from Isilon ...')
 
@@ -34,43 +40,58 @@ def main():
     # TODO: Considering Looping for the case of multiple isilon
     print('ISILON_LOGGER>>> Target Isilon : ' + str_ipaddress)
     print('ISILON_LOGGER>>> General Information : ')
-    get_isilon_information(str_ipaddress, str_username, str_password)
+    isilon_info = get_isilon_information(str_ipaddress, str_username, str_password)
+    # isilon_info['name']
+
+    # Instantiate Collector Class with constructor
+    isilon_collector = Collector(strmark='ISILON')
 
     # Send message to rabbitmq
-    common.send_message('[tmp]Isilon_Start')
+    isilon_collector.send_message('[tmp]Isilon_Start')
 
     # --- Run main task(capacity)
     # Create capacity table in postgres
-    common.create_table()
+    capacity_columns = '(cluster_name varchar' + \
+                       ', timestamp timestamp' + \
+                       ', ifs_bytes_total bigint' + \
+                       ', ifs_bytes_used bigint' + \
+                       ', ifs_percent_used double precision' + \
+                       ', ifs_bytes_free bigint' + \
+                       ', ifs_percent_total double precision)'
+    isilon_collector.create_capacity_table(capacity_columns)
 
     # Get capacity information
-    # common.get_https_response_with_json()
+    uri = 'https://' + str_ipaddress + ':8080' + '/platform/1/cluster/config'
+    ret = get_https_response_with_json(str_username, str_password, uri)
+    print(ret)
 
     # Insert capacity information to postgres
-    common.send_data_to_postgres()
+    # common.send_data_to_postgres()
 
     # --- Run main task(quota)
     # Create quota table in postgres
-    common.create_table()
+    quota_columns = '(qid varchar, qusername varchar, qpassword varchar)'
+    isilon_collector.create_quota_table(quota_columns)
 
     # Get quota information
     # common.get_https_response_with_json()
 
     # Insert quota information to postgres
-    common.send_data_to_postgres()
+    # common.send_data_to_postgres()
 
     # --- Run main task(performance)
     # Create performance table in postgres
-    common.create_table()
+    perf_columns = '(pfid varchar, pfsername varchar, pfpassword varchar)'
+    isilon_collector.create_performance_table(perf_columns)
 
     # Get performance information
     # common.get_https_response_with_json()
 
     # Insert performance information to postgres
-    common.send_data_to_postgres()
+    # common.send_data_to_postgres()
 
     # Send message to rabbitmq
-    common.send_message('[tmp]Isilon_End')
+    isilon_collector.send_message('[tmp]Isilon_END')
 
     print('ISILON_LOGGER>>> Isilon Collector has done its task...!!')
 
