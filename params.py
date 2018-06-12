0efa3b7b15fd22771bd142b1a72926b25754590c
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-06-12 18:46:25 +0900

    modified image name of storage controller and renamed source dockerfiles.

--- params.py
+++ params.py
@@ -17,31 +17,28 @@ pg_portmap = {5432: 5432}
 pg_username = 'postgres'
 pg_password = 'postgres'
 pg_database = 'postgres'
-pg_srcimg = 'postgres:latest'
 
 # -- For RabbitMQ
 mq_address = '10.62.130.167'
 mq_ports = [25672, 5672, 5671, 4369, 15672]
 # mq_portmap = {25672: 32776, 5672: 32777, 5671: 32778, 4369: 32779}
 mq_portmap = {25672: 25672, 5672: 5672, 5671: 5671, 4369: 4369, 15672: 15672}
-mq_srcimg = 'rabbitmq:latest'
 mq_quename = 'smetric_queue'
 
 # -- For ElasticSearch
 es_address = '10.62.130.167'
 es_ports = [9200,]
-es_portmap = {9200:9200}
-es_cname = 'smetrics_elasticsearch'
+es_portmap = {9200: 9200}
 index = {
-    'xtremio':'xtremio',
-    'isilon':'isilon'
+    'xtremio': 'xtremio',
+    'isilon': 'isilon'
 }
 
 # -- For XtremIO
 xtremio_address = '10.32.210.156'
 xtremio_user = 'admin'
 xtremio_pass = 'Xtrem10'
-xtremio_imgname = 'smetric/xtremiocollector'
+xtremio_imgname = 'smetrics/xtremiocollector'
 xtremio_clogpath = '/var/log/collector/xtremio'
 xtremio_hlogpath = '/home/smetric/log/xtremio'
 
@@ -49,6 +46,6 @@ xtremio_hlogpath = '/home/smetric/log/xtremio'
 isilon_address = '10.32.239.181'
 isilon_user = 'root'
 isilon_pass = 'nasadmin'
-isilon_imgname = 'smetric/isiloncollector'
+isilon_imgname = 'smetrics/isiloncollector'
 isilon_clogpath = '/var/log/collector/isilon'
 isilon_hlogpath = '/home/smetric/log/isilon'
