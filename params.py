ea72547ded712e49608bfcc0296ba1ebe1d90198
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-06-13 19:46:06 +0900

    modified quename in rabbitmq, commented out currently unused parameters.

--- params.py
+++ params.py
@@ -4,10 +4,8 @@
 docker_ip = '10.62.130.167'
 docker_port = 2375
 docker_api_version = '1.37'
-
-max_running = 3
-timeout = 100000
-
+# max_running = 3
+# timeout = 100000
 
 # -- For Postgres
 pg_address = '10.62.130.167'
@@ -23,7 +21,7 @@ mq_address = '10.62.130.167'
 mq_ports = [25672, 5672, 5671, 4369, 15672]
 # mq_portmap = {25672: 32776, 5672: 32777, 5671: 32778, 4369: 32779}
 mq_portmap = {25672: 25672, 5672: 5672, 5671: 5671, 4369: 4369, 15672: 15672}
-mq_quename = 'smetric_queue'
+mq_quename = 'smetrics_queue'
 
 # -- For ElasticSearch
 es_address = '10.62.130.167'
@@ -39,13 +37,13 @@ xtremio_address = '10.32.210.156'
 xtremio_user = 'admin'
 xtremio_pass = 'Xtrem10'
 xtremio_imgname = 'smetrics/xtremiocollector'
-xtremio_clogpath = '/var/log/collector/xtremio'
-xtremio_hlogpath = '/home/smetric/log/xtremio'
+# xtremio_clogpath = '/var/log/collector/xtremio'
+# xtremio_hlogpath = '/home/smetric/log/xtremio'
 
 # -- For Isilon
 isilon_address = '10.32.239.181'
 isilon_user = 'root'
 isilon_pass = 'nasadmin'
 isilon_imgname = 'smetrics/isiloncollector'
-isilon_clogpath = '/var/log/collector/isilon'
-isilon_hlogpath = '/home/smetric/log/isilon'
+# isilon_clogpath = '/var/log/collector/isilon'
+# isilon_hlogpath = '/home/smetric/log/isilon'
